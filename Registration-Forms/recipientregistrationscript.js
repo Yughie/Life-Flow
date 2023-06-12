5ff7e27d5261d84971b278a22c67e641b9256ee0
Angela Paco <janice.paco@gmail.com>
2023-06-12 14:25:33 +0800

    checks if username is already taken

--- Registration-Forms/recipientregistrationscript.js
+++ Registration-Forms/recipientregistrationscript.js
@@ -1,1 +1,8 @@
-
+// slide in/out sidenav
+function openSidebar(selected) {
+    if(selected) {
+        document.getElementById("sidenav").style.right = "0px";
+    } else {
+        document.getElementById("sidenav").style.right = "";
+    }
+}
